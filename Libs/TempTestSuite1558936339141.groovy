fb82ec5e35dfee373edf9b6b69afed3864603a16
USER PC <USER PC@DELL>
2019-06-21 17:55:08 +0530

    Katalon_Scripts_Naveen

---
+++ Libs/TempTestSuite1558936339141.groovy
@@ -0,0 +1,35 @@
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.exception.StepFailedException
+import com.kms.katalon.core.reporting.ReportUtil
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.testdata.TestDataColumn
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import internal.GlobalVariable as GlobalVariable
+
+Map<String, String> suiteProperties = new HashMap<String, String>();
+
+
+suiteProperties.put('id', 'Test Suites/Test_suite_1')
+
+suiteProperties.put('name', 'Test_suite_1')
+
+suiteProperties.put('description', '')
+ 
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+
+
+
+RunConfiguration.setExecutionSettingFile("C:\\Users\\USER PC\\Katalon Studio\\eudg\\Reports\\Test_suite_1\\20190527_112219\\execution.properties")
+
+TestCaseMain.beforeStart()
+
+TestCaseMain.startTestSuite('Test Suites/Test_suite_1', suiteProperties, [new TestCaseBinding('Test Cases/defect_11', 'Test Cases/defect_11',  null), new TestCaseBinding('Test Cases/Test_case_1', 'Test Cases/Test_case_1',  null), new TestCaseBinding('Test Cases/Test_case_2', 'Test Cases/Test_case_2',  null)])
---
+++ Libs/TempTestSuite1558936340250.groovy
@@ -0,0 +1,35 @@
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.exception.StepFailedException
+import com.kms.katalon.core.reporting.ReportUtil
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.testdata.TestDataColumn
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import internal.GlobalVariable as GlobalVariable
+
+Map<String, String> suiteProperties = new HashMap<String, String>();
+
+
+suiteProperties.put('id', 'Test Suites/Test_suite_2')
+
+suiteProperties.put('name', 'Test_suite_2')
+
+suiteProperties.put('description', '')
+ 
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+
+
+
+RunConfiguration.setExecutionSettingFile("C:\\Users\\USER PC\\Katalon Studio\\eudg\\Reports\\Test_suite_2\\20190527_112220\\execution.properties")
+
+TestCaseMain.beforeStart()
+
+TestCaseMain.startTestSuite('Test Suites/Test_suite_2', suiteProperties, [new TestCaseBinding('Test Cases/defect_13', 'Test Cases/defect_13',  null), new TestCaseBinding('Test Cases/defect_14', 'Test Cases/defect_14',  null), new TestCaseBinding('Test Cases/defect_26', 'Test Cases/defect_26',  null)])
